a94618a36f24372f0e979b8d745feca747e06ad7
T1m1 <Ruprecht.t@gmx.de>
2015-10-06 15:23:49 +0200

    implement default user service functionality

--- microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/model/User.java
+++ microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/model/User.java
@@ -8,5 +8,10 @@ public class User {
 	private String userId;
 	private String lastname;
 	private String firstname;
-	
+
+	public User(String userId, String firstname, String lastname) {
+		this.userId = userId;
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
 }
--- microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/service/UserService.java
+++ microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/service/UserService.java
@@ -1,9 +1,16 @@
 package com.seitenbau.microservices.core.user.service;
 
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.seitenbau.microservices.core.user.model.User;
 
 @RestController
 public class UserService {
-
+	@RequestMapping("/users/{userId}")
+    public User getProduct(@PathVariable String userId) {		
+        return new User(userId, "Max", "Mustermann");
+    }
 
 }
