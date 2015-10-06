3080d0c4d0f13fae5e1d094c34c49d74ef348c68
T1m1 <Ruprecht.t@gmx.de>
2015-10-06 16:47:03 +0200

    rename package

--- microservices/core/document-service/src/main/java/com/seitenbau/microservices/core/message/DocumentServiceApplication.java
+++ microservices/core/document-service/src/main/java/com/seitenbau/microservices/core/message/DocumentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.seitenbau.microservices.core.document;
+package com.seitenbau.microservices.core.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
