04a2e879b8c0d66b41bf5190c6e9036b0f025d2d
T1m1 <Ruprecht.t@gmx.de>
2015-10-06 15:00:02 +0200

    create user model

---
+++ microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/model/User.java
@@ -0,0 +1,12 @@
+package com.seitenbau.microservices.core.user.model;
+
+import lombok.Data;
+
+@Data
+public class User {
+
+	private String userId;
+	private String lastname;
+	private String firstname;
+	
+}
