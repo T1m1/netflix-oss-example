8e06d10fd24aa5005002291fe7302396539aa33c
T1m1 <Ruprecht.t@gmx.de>
2015-10-07 09:57:44 +0200

    implement discovery registration to message-service

--- microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/MessageServiceApplication.java
+++ microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/MessageServiceApplication.java
@@ -2,8 +2,10 @@ package com.seitenbau.microservices.core.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MessageServiceApplication {
 
     public static void main(String[] args) {
