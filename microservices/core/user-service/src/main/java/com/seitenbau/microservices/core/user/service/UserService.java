81b4e13af0ab278e3ca3029c9b0c7f1ba81f9ccd
T1m1 <Ruprecht.t@gmx.de>
2015-10-07 11:18:09 +0200

    impelment status method

--- microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/service/UserService.java
+++ microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/service/UserService.java
@@ -1,5 +1,7 @@
 package com.seitenbau.microservices.core.user.service;
 
+import java.util.Date;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +14,11 @@ public class UserService {
 	@RequestMapping("/users/{userId}")
     public User getUser(@PathVariable String userId) {		
         return new User(userId, "Max", "Mustermann");
+    }
+	
+	@RequestMapping("/info")
+    public String getStatus() {		
+		 return "{\"timestamp\":\"" + new Date() + "\",\"content\":\"I'm okay ;-)\"}";
     }
 
 }
