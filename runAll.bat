f1420e1b2eebe04bb69c750506eeb8e1106e44b3
T1m1 <Ruprecht.t@gmx.de>
2015-10-07 10:21:41 +0200

    add missing servies to run all bat

--- runAll.bat
+++ runAll.bat
@@ -1,3 +1,9 @@
+:: support
+start /D "microservices/support/discovery-service" mvn spring-boot:run
+
+
 :: core
 start /D "microservices/core/user-service" mvn spring-boot:run
 start /D "microservices/core/document-service" mvn spring-boot:run
+start /D "microservices/core/message-service" mvn spring-boot:run
+
