631c688fec9309e3e290b635c90cb6e1f2c776a8
T1m1 <Ruprecht.t@gmx.de>
2015-10-08 08:00:32 +0200

    create message integration class

--- microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/service/MailboxService.java
+++ microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/service/MailboxService.java
@@ -2,6 +2,8 @@ package com.seitenbau.microservices.composite.mailbox.service;
 
 import java.util.Date;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +12,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MailboxService {
 	
-	@RequestMapping("/mailboxes/{mailboxId}")
-    public String getMailbox(@PathVariable String mailboxId) {		
-      return null;
-    }
+	
+	@Autowired
+	MessageIntegration messageIntegration;
 	
 	@RequestMapping("/info")
     public String getStatus() {		
 		 return "{\"timestamp\":\"" + new Date() + "\",\"content\":\"I'm okay ;-)\"}";
     }
-
+	
+	@RequestMapping("/mailboxes/{userId}")
+    public String getMailbox(@PathVariable String userId) {
+		
+		// 1. get all messages of user with userId
+//		ResponseEntity<Message> messages = 
+		
+      return null;
+    }
 }
---
+++ microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/service/MessageIntegration.java
@@ -0,0 +1,8 @@
+package com.seitenbau.microservices.composite.mailbox.service;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageIntegration {
+
+}
