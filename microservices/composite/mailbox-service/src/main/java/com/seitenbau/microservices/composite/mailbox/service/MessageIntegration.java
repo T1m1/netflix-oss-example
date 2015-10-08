9518ddf07887a43a33982c5c2c2ec31d04d992ef
T1m1 <Ruprecht.t@gmx.de>
2015-10-08 10:46:17 +0200

    optimize imports

--- microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/service/MessageIntegration.java
+++ microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/service/MessageIntegration.java
@@ -5,8 +5,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.appinfo.InstanceInfo;
