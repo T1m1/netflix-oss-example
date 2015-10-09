f7b694342206c68ea907e16cc894cdc44131c478
T1m1 <Ruprecht.t@gmx.de>
2015-10-09 10:10:24 +0200

    add request method to function

--- microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/service/UserService.java
+++ microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/service/UserService.java
@@ -4,21 +4,23 @@ import java.util.Date;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.seitenbau.microservices.core.user.model.User;
 
 @RestController
 public class UserService {
-	
-	@RequestMapping("/users/{userId}")
-    public User getUser(@PathVariable String userId) {		
-        return new User(userId, "Max", "Mustermann");
-    }
-	
+
+	@RequestMapping(value = "/users/{userId}", method = RequestMethod.GET)
+	public User getUser(@PathVariable String userId) {
+		return new User(userId, "Max", "Mustermann");
+	}
+
 	@RequestMapping("/info")
-    public String getStatus() {		
-		 return "{\"timestamp\":\"" + new Date() + "\",\"content\":\"I'm okay ;-)\"}";
-    }
+	public String getStatus() {
+		return "{\"timestamp\":\"" + new Date()
+				+ "\",\"content\":\"I'm okay ;-)\"}";
+	}
 
 }
