76ba03ab002e0078a77cb11e0e54a26ca150bcdd
T1m1 <Ruprecht.t@gmx.de>
2015-10-09 15:18:25 +0200

    add response Funtion

--- microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/service/MailboxService.java
+++ microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/service/MailboxService.java
@@ -1,5 +1,6 @@
 package com.seitenbau.microservices.composite.mailbox.service;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedHashSet;
@@ -7,11 +8,13 @@ import java.util.List;
 import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.seitenbau.microservices.composite.mailbox.model.MailboxEntry;
 import com.seitenbau.microservices.core.message.model.Message;
 import com.seitenbau.microservices.core.user.model.User;
 
@@ -35,15 +38,17 @@ public class MailboxService {
 	 * @return
 	 */
 	@RequestMapping("/mailboxes/{userId}")
-	public ResponseEntity<List<Message>> getMailbox(@PathVariable String userId) {
+	public ResponseEntity<List<MailboxEntry>> getMailbox(@PathVariable String userId) {
 
+		List<MailboxEntry> mailboxEntries = new ArrayList<MailboxEntry>();
+		
 		// 1. get all messages of user with userId
 		ResponseEntity<List<Message>> messages = mailboxIntegration
 				.getMessagesSentToUser(userId);
 
 		// return empty list if no messages found
 		if (!messages.getStatusCode().is2xxSuccessful()) {
-			return messages;
+			return createResponse(mailboxEntries, messages.getStatusCode());
 		}
 
 		// 2. generate unique list with users
@@ -66,9 +71,14 @@ public class MailboxService {
 			}
 		}
 
-		System.out.println(allUser.toString());
+		// TODO get documents
+		// TODO create message entry object
 
-		return messages;
+		return createResponse(mailboxEntries, messages.getStatusCode());
+	}
+	
+	public <T> ResponseEntity<T> createResponse(T body, HttpStatus httpStatus) {
+		return new ResponseEntity<>(body, httpStatus);
 	}
 
 }
