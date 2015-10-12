c83dfd0c94ec64de5233e7c92b9c6d474b8e9c7a
T1m1 <Ruprecht.t@gmx.de>
2015-10-12 16:06:02 +0200

    apiv1 routing and simple application start

--- microservices/support/edge-server/src/main/java/com/seitenbau/microservices/support/EdgeServerApplication.java
+++ microservices/support/edge-server/src/main/java/com/seitenbau/microservices/support/EdgeServerApplication.java
@@ -2,18 +2,13 @@ package com.seitenbau.microservices.support;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.stereotype.Controller;
 
 @SpringBootApplication
-@Controller
 @EnableZuulProxy
 public class EdgeServerApplication {
 
-	public static void main(String[] args) {
-		new SpringApplicationBuilder(EdgeServerApplication.class).web(true)
-				.run(args);
-		// SpringApplication.run(EdgeServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EdgeServerApplication.class, args);
+    }
 }
