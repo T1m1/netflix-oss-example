bb561f523a5c2b73dc29ca8c96b8f7ebb2ca3ce4
T1m1 <Ruprecht.t@gmx.de>
2015-10-12 09:38:35 +0200

    add attachment to message model

--- microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/model/Message.java
+++ microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/model/Message.java
@@ -1,5 +1,7 @@
 package com.seitenbau.microservices.core.message.model;
 
+import java.util.List;
+
 import lombok.Data;
 
 @Data
@@ -10,16 +12,18 @@ public class Message {
 	private String fromId;
 	private String message;
 	private String subject;
+	private List<String> attachment;
 
 	public Message() {
 	}
 
 	public Message(String messageId, String toId, String fromId,
-			String message, String subject) {
+			String message, String subject, List<String> attachment) {
 		this.messageId = messageId;
 		this.toId = toId;
 		this.fromId = fromId;
 		this.message = message;
 		this.subject = subject;
+		this.attachment = attachment;
 	}
 }
