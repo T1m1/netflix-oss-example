b5158de2f55f891eb8fbf1eab2ff821de28ac245
T1m1 <Ruprecht.t@gmx.de>
2015-10-12 09:10:51 +0200

    add header for message service

--- microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/service/MessageService.java
+++ microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/service/MessageService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.seitenbau.microservices.core.message.model.Message;
@@ -12,7 +13,7 @@ import com.seitenbau.microservices.core.message.model.Message;
 @RestController
 public class MessageService {
 
-	@RequestMapping("/messages/{messageId}")
+	@RequestMapping(value = "/messages/{messageId}", method = RequestMethod.GET, produces = "application/json")
 	public List<Message> getMessage(@PathVariable String messageId) {
 		List<Message> msg = new ArrayList<Message>();
 		msg.add(new Message(messageId, "1a2b3c4e", "0z1y2x3w", "Hello Word",
