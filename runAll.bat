e7149ed028b06d43b5721deb69f784a35444df09
T1m1 <Ruprecht.t@gmx.de>
2015-10-13 16:01:51 +0200

    start all mongodb instances

--- runAll.bat
+++ runAll.bat
@@ -1,6 +1,22 @@
-:: mongodb
-if not exist "C:/data/db-user" mkdir "C:/data/db-user"
-start /D "C:/" mongod --dbpath "C:/data/db-user" --port 1001
+:: PORTS
+SET MONGO_DB_PORT_USER=1001
+SET MONGO_DB_PORT_MESSAGE=1002
+SET MONGO_DB_PORT_DOCUMENT=1003
+
+:: PATHS
+SET MONGO_DB_PATH_USER="C:/data/db-user"
+SET MONGO_DB_PATH_MESSAGE="C:/data/db-message"
+SET MONGO_DB_PATH_DOCUMENT="C:/data/db-document"
+
+:: mongodb - user
+if not exist %MONGO_DB_PATH_USER% mkdir %MONGO_DB_PATH_USER%
+start /D "C:/" mongod --dbpath %MONGO_DB_PATH_USER% --port %MONGO_DB_PORT_USER%
+:: mongodb - message
+if not exist %MONGO_DB_PATH_MESSAGE% mkdir %MONGO_DB_PATH_MESSAGE%
+start /D "C:/" mongod --dbpath %MONGO_DB_PATH_MESSAGE% --port %MONGO_DB_PORT_MESSAGE%
+:: mongodb - user
+if not exist %MONGO_DB_PATH_DOCUMENT% mkdir %MONGO_DB_PATH_DOCUMENT%
+start /D "C:/" mongod --dbpath %MONGO_DB_PATH_DOCUMENT% --port %MONGO_DB_PORT_DOCUMENT%
 
 :: support
 start /D "microservices/support/discovery-server" mvn spring-boot:run
