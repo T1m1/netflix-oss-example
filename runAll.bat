bea5ddd009ac650eeba5582758eaa886323f96ea
T1m1 <Ruprecht.t@gmx.de>
2015-10-13 15:18:28 +0200

    bugfix - missing quotes

--- runAll.bat
+++ runAll.bat
@@ -1,6 +1,6 @@
 :: mongodb
-if not exist "C:/data/db-user" mkdir C:/data/db-user
-start /D "C:/" mongod --dbpath C:/data/db-user --port 1001
+if not exist "C:/data/db-user" mkdir "C:/data/db-user"
+start /D "C:/" mongod --dbpath "C:/data/db-user" --port 1001
 
 :: support
 start /D "microservices/support/discovery-server" mvn spring-boot:run
