fc2aec6c0040058fd172736efc8ce78006015442
T1m1 <Ruprecht.t@gmx.de>
2015-10-13 14:57:00 +0200

    add id annotation to model

--- microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/model/User.java
+++ microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/model/User.java
@@ -1,20 +1,21 @@
 package com.seitenbau.microservices.core.user.model;
 
+import org.springframework.data.annotation.Id;
+
 import lombok.Data;
 
 @Data
 public class User {
 
-	private String userId;
-	private String lastname;
-	private String firstname;
+	@Id private String userId;
+	private String lastName;
+	private String firstName;
 
 	public User() {
 	}
 
-	public User(String userId, String firstname, String lastname) {
-		this.userId = userId;
-		this.firstname = firstname;
-		this.lastname = lastname;
+	public User(String firstName, String lastName) {
+		this.firstName = firstName;
+		this.lastName = lastName;
 	}
 }
