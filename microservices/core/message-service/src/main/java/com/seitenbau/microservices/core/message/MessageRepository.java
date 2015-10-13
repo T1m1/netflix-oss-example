e788a8bd5e0569df7e09cf0b96520eee06fca716
T1m1 <Ruprecht.t@gmx.de>
2015-10-13 15:42:49 +0200

    refactor message repository to new REST urls

--- microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/MessageRepository.java
+++ microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/MessageRepository.java
@@ -1,11 +1,15 @@
 package com.seitenbau.microservices.core.message;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.seitenbau.microservices.core.message.model.Message;
 
-
 public interface MessageRepository extends MongoRepository<Message, String> {
 
+	public List<Message> findByToId(String toId);
+
+	public List<Message> findByFromId(String fromId);
 
 }
--- microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/service/MessageService.java
+++ microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/service/MessageService.java
@@ -1,24 +1,29 @@
 package com.seitenbau.microservices.core.message.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.seitenbau.microservices.core.message.MessageRepository;
 import com.seitenbau.microservices.core.message.model.Message;
 
 @RestController
 public class MessageService {
 
-	@RequestMapping(value = "/messages/{messageId}", method = RequestMethod.GET, produces = "application/json")
-	public List<Message> getMessage(@PathVariable String messageId) {
-		List<Message> msg = new ArrayList<Message>();
-		msg.add(new Message(messageId, "1a2b3c4e", "0z1y2x3w", "Hello Word",
-				"Huhu", Arrays.asList("id123", "id234")));
-		return msg;
+	@Autowired
+	private MessageRepository repository;
+
+	@RequestMapping(value = "/outbox/{userId}", method = RequestMethod.GET, produces = "application/json")
+	public List<Message> getOutboxFromUser(@PathVariable String userId) {
+		return repository.findByFromId(userId);
+	}
+	
+	@RequestMapping(value = "/inbox/{userId}", method = RequestMethod.GET, produces = "application/json")
+	public List<Message> getInboxFromUser(@PathVariable String userId) {
+		return repository.findByToId(userId);
 	}
 }
