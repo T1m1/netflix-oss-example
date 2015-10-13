804a46abc96e171ecc389728f130efcaebbc776b
T1m1 <Ruprecht.t@gmx.de>
2015-10-13 08:49:45 +0200

    add missing services

--- runAll.bat
+++ runAll.bat
@@ -1,9 +1,13 @@
 :: support
-start /D "microservices/support/discovery-service" mvn spring-boot:run
-
+start /D "microservices/support/discovery-server" mvn spring-boot:run
+start /D "microservices/support/edge-server" mvn spring-boot:run
 
 :: core
 start /D "microservices/core/user-service" mvn spring-boot:run
 start /D "microservices/core/document-service" mvn spring-boot:run
 start /D "microservices/core/message-service" mvn spring-boot:run
+
+:: composite
+start /D "microservices/composite/mailbox-service" mvn spring-boot:run
+
 
