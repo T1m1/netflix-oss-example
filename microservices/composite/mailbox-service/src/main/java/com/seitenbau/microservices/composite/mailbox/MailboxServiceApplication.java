165f15c9a03e31c4271c552a708f8011d65afd68
T1m1 <Ruprecht.t@gmx.de>
2015-10-13 10:59:57 +0200

    enable circuit breaker

--- microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/MailboxServiceApplication.java
+++ microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/MailboxServiceApplication.java
@@ -2,10 +2,12 @@ package com.seitenbau.microservices.composite.mailbox;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 public class MailboxServiceApplication {
 
     public static void main(String[] args) {
