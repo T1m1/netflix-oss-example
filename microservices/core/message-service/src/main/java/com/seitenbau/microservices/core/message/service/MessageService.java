c84a46f23eef8e5c83d223974fd941057fd7d745
T1m1 <Ruprecht.t@gmx.de>
2015-10-14 09:52:46 +0200

    implement CRUD methods

--- microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/service/MessageService.java
+++ microservices/core/message-service/src/main/java/com/seitenbau/microservices/core/message/service/MessageService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import com.seitenbau.microservices.core.message.MessageRepository;
 import com.seitenbau.microservices.core.message.model.Message;
 
 @RestController
+@RequestMapping("messages")
 public class MessageService {
 
 	@Autowired
@@ -21,9 +23,37 @@ public class MessageService {
 	public List<Message> getOutboxFromUser(@PathVariable String userId) {
 		return repository.findByFromId(userId);
 	}
-	
+
 	@RequestMapping(value = "/inbox/{userId}", method = RequestMethod.GET, produces = "application/json")
 	public List<Message> getInboxFromUser(@PathVariable String userId) {
 		return repository.findByToId(userId);
 	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Message> getAllMessages() {
+		return repository.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void create(Message message) {
+		repository.save(message);
+	}
+
+	@RequestMapping(value = "{messageId}", method = RequestMethod.DELETE, consumes = "application/json")
+	public void delete(@PathVariable String messageId) {
+		repository.delete(messageId);
+	}
+
+	@RequestMapping(value = "{messageId}", method = RequestMethod.PUT, consumes = "application/json")
+	public Message update(@PathVariable String messageId,
+			@RequestBody Message message) {
+		Message update = repository.findOne(messageId);
+		update.setFromId(message.getFromId());
+		update.setToId(message.getToId());
+		update.setMessage(message.getMessage());
+		update.setSubject(message.getSubject());
+		update.setAttachmentIds(message.getAttachmentIds());
+		return repository.save(update);
+	}
+
 }
