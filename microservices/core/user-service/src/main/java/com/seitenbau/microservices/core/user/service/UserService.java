7b3bd9fbf7339144761a1e7ba061c51a8c427ea0
T1m1 <Ruprecht.t@gmx.de>
2015-10-14 09:07:22 +0200

    add CRUD methods to user

--- microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/service/UserService.java
+++ microservices/core/user-service/src/main/java/com/seitenbau/microservices/core/user/service/UserService.java
@@ -1,11 +1,11 @@
 package com.seitenbau.microservices.core.user.service;
 
 import java.util.Date;
-import java.util.UUID;
+import java.util.List;
 
-import org.neo4j.cypher.internal.compiler.v2_1.perty.docbuilders.toStringDocBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,33 +14,39 @@ import com.seitenbau.microservices.core.user.UserRepository;
 import com.seitenbau.microservices.core.user.model.User;
 
 @RestController
+@RequestMapping("/users")
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repository;
-
-	@RequestMapping(value = "/users/{userId}", method = RequestMethod.GET)
-	public User getUser(@PathVariable String userId) {
-		return repository.findOne(userId);
-	}
 
 	@RequestMapping("/info")
 	public String getStatus() {
 		return "{\"timestamp\":\"" + new Date()
 				+ "\",\"content\":\"I'm okay ;-)\"}";
 	}
-	
-	
-	@RequestMapping(value = "/test/{testname}", method = RequestMethod.POST)
-	public void testDB(@PathVariable String testname) {
-		repository.save(new User(testname, testname));
-		for (User user : repository.findAll()) {
-			System.out.println(user.getFirstName() + user.getLastName());
-		}
-		
-		System.out.println("Find user with name: " + testname);
-		System.out.println(repository.findByFirstName(testname));
-		
+
+	@RequestMapping(value = "{userId}", method = RequestMethod.GET)
+	public User getUser(@PathVariable String userId) {
+		return repository.findOne(userId);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public List<User> getAll() {
+		return repository.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "{userId}")
+	public void delete(@PathVariable String userId) {
+		repository.delete(userId);
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "{userId}")
+	public User update(@PathVariable String userId, @RequestBody User user) {
+		User update = repository.findOne(userId);
+		update.setFirstName(user.getFirstName());
+		update.setLastName(user.getLastName());
+		return repository.save(update);
 	}
 
 }
