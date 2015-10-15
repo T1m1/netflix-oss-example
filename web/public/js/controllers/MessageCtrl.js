c8247fdc2a2a3c7ee053287b809caf6391173d00
T1m1 <Ruprecht.t@gmx.de>
2015-10-15 18:21:42 +0200

    implement functionality to send messages from user x to y

--- web/public/js/controllers/MessageCtrl.js
+++ web/public/js/controllers/MessageCtrl.js
@@ -1,13 +1,25 @@
 angular.module('MessageCtrl', []).controller('MessageController', function ($scope, Message) {
 
-    $scope.sendMessage = function (message) {
-
-        Message.sendMessage(message).then(function success(response) {
-            $scope.messages = response.data;
-            console.log('success');
+    $scope.sendMessage = function () {
+        var msg = {};
+        msg.fromId =  $scope.fromUser.userId;
+        msg.toId = $scope.toUser.userId;
+        msg.subject = $scope.subject;
+        msg.message = $scope.message;
+        console.log(msg);
+        Message.sendMessage(msg).then(function success(response) {
+            console.log(response)
         }).catch(function errorCallback(err) {
             console.log(err);
         });
-    }
+    };
+
+    $scope.selectFromUser = function (fromUser) {
+        $scope.fromUser = fromUser;
+    };
+
+    $scope.selectToUser = function (toUser) {
+        $scope.toUser = toUser;
+    };
 
 });
