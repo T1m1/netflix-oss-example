5db067b13ef305e73534ac8556eee48a48168e35
T1m1 <Ruprecht.t@gmx.de>
2015-10-15 09:05:06 +0200

    add dashboard service project

---
+++ microservices/support/dashboard-service/src/main/java/com/seitenbau/microservices/support/DashboardServiceApplication.java
@@ -0,0 +1,12 @@
+package com.seitenbau.microservices.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardServiceApplication.class, args);
+    }
+}
