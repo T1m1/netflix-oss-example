a4335da7e5cd113e9d8248c629392d848b505236
T1m1 <Ruprecht.t@gmx.de>
2015-10-15 09:48:51 +0200

    implement hsystrix dashboard

--- microservices/support/dashboard-service/src/main/java/com/seitenbau/microservices/support/DashboardServiceApplication.java
+++ microservices/support/dashboard-service/src/main/java/com/seitenbau/microservices/support/DashboardServiceApplication.java
@@ -2,8 +2,10 @@ package com.seitenbau.microservices.support;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableHystrixDashboard 
 public class DashboardServiceApplication {
 
     public static void main(String[] args) {
