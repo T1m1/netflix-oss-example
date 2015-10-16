81acd8dc024eeb64c31c8fc0d6e7513f563a81cf
T1m1 <Ruprecht.t@gmx.de>
2015-10-16 14:50:06 +0200

    refactor mailbox service - create util class

--- microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/service/MailboxIntegration.java
+++ microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/service/MailboxIntegration.java
@@ -2,17 +2,11 @@ package com.seitenbau.microservices.composite.mailbox.service;
 
 import java.util.List;
 
-import javax.annotation.PostConstruct;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.ServiceInstance;
-import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.seitenbau.microservices.core.document.model.Document;
@@ -26,14 +20,7 @@ import com.seitenbau.microservices.core.user.model.User;
 public class MailboxIntegration {
 
 	@Autowired
-	private LoadBalancerClient loadBalancer;
-
-	private RestTemplate restTemplate;
-
-	@PostConstruct
-	private void init() {
-		this.restTemplate = new RestTemplate();
-	}
+	private Util util;
 
 	/**
 	 * Call message-service to get all messages with specific user id
@@ -42,25 +29,21 @@ public class MailboxIntegration {
 	 * @return all message of user with userId
 	 */
 	public ResponseEntity<List<Message>> getMessagesSentToUser(String userId) {
-		return getResponseAsList("message-service", "/messages/inbox/" + userId,
-				new ParameterizedTypeReference<List<Message>>() {
-				});
+		return util.getResponseAsList("message-service", "/messages/inbox/"
+				+ userId, new ParameterizedTypeReference<List<Message>>() {
+		});
 	}
 
 	/**
 	 * Call user-service to get user with userId
-	 * 
-	 * NOTE: Function body can replaces with method call of
-	 * "getResponseAsObject". Only for demonstration of hystrix.
 	 * 
 	 * @param userId
 	 * @return all message of user with userId
 	 */
 	@HystrixCommand(fallbackMethod = "defaultUsers")
 	public ResponseEntity<User> getUser(String userId) {
-		String url = getRequestUrl("user-service", "/users/" + userId);
-		User obj = restTemplate.getForObject(url, User.class);
-		return createResponse(obj, HttpStatus.OK);
+		return util.getResponseAsObject("user-service", "/users/" + userId,
+				User.class);
 	}
 
 	/**
@@ -70,7 +53,7 @@ public class MailboxIntegration {
 	 * @return
 	 */
 	public ResponseEntity<User> defaultUsers(String userId) {
-		return createResponse(null, HttpStatus.BAD_GATEWAY);
+		return util.createResponse(null, HttpStatus.BAD_GATEWAY);
 	}
 
 	/**
@@ -80,46 +63,8 @@ public class MailboxIntegration {
 	 * @return
 	 */
 	public ResponseEntity<Document> getDocument(String documentId) {
-		return getResponseAsObject("document-service", "/documents/"
+		return util.getResponseAsObject("document-service", "/documents/"
 				+ documentId, Document.class);
 	}
 
-	private <T> ResponseEntity<T> getResponseAsList(String serviceId,
-			String requestURI,
-			ParameterizedTypeReference<T> parameterizedTypeReference) {
-		try {
-			String url = getRequestUrl(serviceId, requestURI);
-			return restTemplate.exchange(url, HttpMethod.GET, null,
-					parameterizedTypeReference);
-		} catch (Exception e) {
-			System.out.println(e);
-			return createResponse(null, HttpStatus.SERVICE_UNAVAILABLE);
-		}
-	}
-
-	private <T> ResponseEntity<T> getResponseAsObject(String serviceId,
-			String requestURI, Class<T> type) {
-		try {
-			String getUserURL = getRequestUrl(serviceId, requestURI);
-			// get user information
-			T obj = restTemplate.getForObject(getUserURL, type);
-
-			return createResponse(obj, HttpStatus.OK);
-		} catch (Exception e) {
-			System.out.println(e);
-			return createResponse(null, HttpStatus.SERVICE_UNAVAILABLE);
-		}
-	}
-
-	private String getRequestUrl(String serviceId, String requestURI) {
-		// get service URL for message-service
-		ServiceInstance instance = loadBalancer.choose(serviceId);
-
-		// build request URL
-		return instance.getUri() + requestURI;
-	}
-
-	private <T> ResponseEntity<T> createResponse(T body, HttpStatus httpStatus) {
-		return new ResponseEntity<>(body, httpStatus);
-	}
 }
---
+++ microservices/composite/mailbox-service/src/main/java/com/seitenbau/microservices/composite/mailbox/service/Util.java
@@ -0,0 +1,71 @@
+package com.seitenbau.microservices.composite.mailbox.service;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class Util {
+
+	private static final Logger LOG = LoggerFactory.getLogger(Util.class);
+
+	@Autowired
+	private LoadBalancerClient loadBalancer;
+
+	private RestTemplate restTemplate;
+
+	@PostConstruct
+	private void init() {
+		this.restTemplate = new RestTemplate();
+	}
+
+	protected <T> ResponseEntity<T> createResponse(T body, HttpStatus httpStatus) {
+		return new ResponseEntity<>(body, httpStatus);
+	}
+
+	protected <T> ResponseEntity<T> getResponseAsObject(String serviceId,
+			String requestURI, Class<T> type) {
+		try {
+			String getUserURL = getRequestUrl(serviceId, requestURI);
+			T obj = restTemplate.getForObject(getUserURL, type);
+
+			return createResponse(obj, HttpStatus.OK);
+		} catch (Exception e) {
+			LOG.warn("Failed to resolve serviceId '{}' with requestURI '{}",
+					serviceId, requestURI);
+			return createResponse(null, HttpStatus.SERVICE_UNAVAILABLE);
+		}
+	}
+
+	protected <T> ResponseEntity<T> getResponseAsList(String serviceId,
+			String requestURI,
+			ParameterizedTypeReference<T> parameterizedTypeReference) {
+		try {
+			String url = getRequestUrl(serviceId, requestURI);
+
+			return restTemplate.exchange(url, HttpMethod.GET, null,
+					parameterizedTypeReference);
+		} catch (Exception e) {
+			LOG.warn("Failed to resolve serviceId '{}' with requestURI '{}",
+					serviceId, requestURI);
+			return createResponse(null, HttpStatus.SERVICE_UNAVAILABLE);
+		}
+	}
+
+	protected String getRequestUrl(String serviceId, String requestURI) {
+		// get service URL for message-service
+		ServiceInstance instance = loadBalancer.choose(serviceId);
+		// build request URL
+		return instance.getUri() + requestURI;
+	}
+}
