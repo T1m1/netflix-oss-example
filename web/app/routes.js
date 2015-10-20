7a06cf81147d9b1f836dcf417a6c75b834ed9d99
T1m1 <Ruprecht.t@gmx.de>
2015-10-20 16:14:49 +0200

    add workaround for angular bug

--- web/app/routes.js
+++ web/app/routes.js
@@ -6,7 +6,7 @@ module.exports = function (app) {
 
     // frontend routes =========================================================
     // route to handle all angular requests
-    app.get('*', function (req, res) {
+    app.get('/*', function (req, res) {
         res.sendfile('./public/index.html');
     });
 
--- web/server.js
+++ web/server.js
@@ -18,12 +18,17 @@ app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(bodyParser.urlencoded({extended: true})); // parse application/x-www-form-urlencoded
 
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/bower_components', express.static(__dirname + '/../bower_components'));
+app.use('/css', express.static(__dirname + '/css'));
+
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request. simulate DELETE/PUT
 app.use(express.static(__dirname + '/public')); // set the static files location /public/img will be /img for users
 
 app.get('/hostname', function(req, res) {
    return res.json({host: host})
 });
+
 // routes ==================================================
 require('./app/routes')(app); // pass our application into our routes
 
