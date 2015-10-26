94801277d08286005654a8041085d54c26f42095
T1m1 <Ruprecht.t@gmx.de>
2015-10-26 13:27:55 +0100

    implement autoconfig in document service

--- microservices/core/document-service/src/main/java/com/seitenbau/microservices/core/document/DocumentServiceApplication.java
+++ microservices/core/document-service/src/main/java/com/seitenbau/microservices/core/document/DocumentServiceApplication.java
@@ -1,11 +1,13 @@
 package com.seitenbau.microservices.core.document;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableAutoConfiguration
 public class DocumentServiceApplication {
 
 	public static void main(String[] args) {
