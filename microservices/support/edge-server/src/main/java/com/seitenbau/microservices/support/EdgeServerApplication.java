251850fad37458b6494df98543e2aa97ea0f6435
Timi <T1m1@users.noreply.github.com>
2015-11-27 11:18:58 +0100

    Update EdgeServerApplication.java

--- microservices/support/edge-server/src/main/java/com/seitenbau/microservices/support/EdgeServerApplication.java
+++ microservices/support/edge-server/src/main/java/com/seitenbau/microservices/support/EdgeServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
 @EnableZuulProxy
+@EnableDiscoveryClient 
 @EnableAutoConfiguration
 public class EdgeServerApplication {
 
